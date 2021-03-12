6761caa00bf4d6bef4202da932dd6feca8f100b5
gaofeng <gaofengyin@hotmail.com>
2021-03-12 23:14:59 +0000

    declaring varaible

---
+++ code/variable/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//declaring variable, assign value and print it out
+	var age int = 10
+
+	var salary float64
+
+	salary = 450.55
+
+	var isTrue bool = true
+
+	var name string
+
+	name = "Fedek"
+
+	fmt.Println(age)
+	fmt.Println(salary)
+	fmt.Println(name)
+	fmt.Println(isTrue)
+
+	fmt.Println()
+
+	//short varaible declaration
+	a := 23
+
+	b := "string"
+
+	c := 32.34
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
