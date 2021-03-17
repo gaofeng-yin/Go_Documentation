0e682545c7a06dca0be3d2f3ae414ee872491aca
gaofeng <gaofengyin@hotmail.com>
2021-03-17 09:24:49 +0000

    struct

--- code/struct/main.go
+++ code/struct/main.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
+//person strcut
 type person struct {
 	first string
 	last  string
 	age   int
 }
 
+//constructor for person. Create person with given firstName, lastName and agePerson. It returns a pointer.
 func newPerson(firstName, lastName string, agePerson int) *person {
 	p := person{
 		first: firstName,
@@ -17,24 +19,42 @@ func newPerson(firstName, lastName string, agePerson int) *person {
 	return &p
 }
 
-func (p person) fullname() {
-	fmt.Println("Full name:", p.first, p.last)
+//person function to get fullname. Only accessible for type person.
+func (p person) fullname() string {
+	return p.first + " " + p.last
+}
+
+//to modify person.age we use pointer or it won't be changed
+func (p *person) incAge(i int) {
+	p.age += i
 }
 
 func main() {
 
+	//long way to create person
 	p1 := person{
 		first: "George",
 		last:  "Roman",
 		age:   45,
 	}
 
+	//short way to create person
 	p2 := person{"Cristiano", "Messi", 34}
 
+	//create person using constructor
+	p3 := newPerson("Steve", "Liu", 24)
+
+	//p1 increment age by 10
+	p1.incAge(10)
+
+	//print out person info
 	fmt.Println(p1)
 	fmt.Println(p2)
+	fmt.Println(p3)
 
-	p1.fullname()
-	p2.fullname()
+	//get person full name
+	fmt.Println(p1.fullname())
+	fmt.Println(p2.fullname())
+	fmt.Println(p3.fullname())
 
 }
