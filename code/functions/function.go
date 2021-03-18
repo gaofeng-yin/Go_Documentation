7c940375ef8a972fc8ebf3834662e5db0c87439a
gaofeng <gaofengyin@hotmail.com>
2021-03-18 11:02:00 +0000

    functions

--- code/functions/function.go
+++ code/functions/function.go
@@ -3,25 +3,40 @@ package main
 import "fmt"
 
 func main() {
+	//function greet()
 	greet()
+	//function greetTo. "Yenni" is argument
 	greetTo("Yenni")
 
-	fmt.Println(sum(2, 3))
+	//you can also pass a variable as argument
+	name := "Golang"
 
+	greetTo(name)
+
+	a := 3
+
+	b := 5
+
+	fmt.Println(sum(a, b))
+
+	number := sum(10, 10)
+
+	fmt.Println(number)
 }
 
+//how to define a function : func FUNCTIONAME(PARAMETER)RETURN{ STATEMENTS }
+
+//function without parameter
 func greet() {
 	fmt.Println("Hello world!")
 }
 
+//function with parameter
 func greetTo(name string) {
 	fmt.Println("Hello", name)
 }
 
+//function with 2 parameter and returns a integer
 func sum(a, b int) int {
 	return a + b
 }
-
-// Passing anonymous function as an argument(parameter)
-
-// Returning anonymous function
