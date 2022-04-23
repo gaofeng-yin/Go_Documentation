1e84c7f83d6f1df0e49c7de9324a6a4c89ca6581
gaofeng_yin <gaofeng.yin@glintt.com>
2022-04-23 22:59:57 +0100

    file

---
+++ exercises/format_phone_number/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Numbers struct {
+	Numbers []Number `json:"Numbers"`
+}
+
+type Number struct {
+	Number string `json:"Number"`
+}
+
+func main() {
+	jsonFile, err := os.Open("phoneNumber.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var numbers Numbers
+
+	json.Unmarshal(byteValue, &numbers)
+
+	cretedFile, err := os.Create("formatedNumbers.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer cretedFile.Close()
+
+	for _, value := range numbers.Numbers {
+		cretedFile.WriteString(reformatPhoneNumber(value.Number))
+		cretedFile.WriteString("\n")
+	}
+}
+
+func reformatPhoneNumber(phoneNumber string) string {
+	var cleanNumber string = removeSpacesAndDashes(phoneNumber)
+	var cleanedNumber []string
+
+	cleanedNumber = append(cleanedNumber, string(cleanNumber[0]))
+	for i := 1; i < len(cleanNumber); i++ {
+		if i%3 == 0 {
+			cleanedNumber = append(cleanedNumber, "-")
+		}
+		cleanedNumber = append(cleanedNumber, string(cleanNumber[i]))
+	}
+
+	if len(cleanNumber)%3 == 1 {
+		swapValue(&cleanedNumber[len(cleanedNumber)-3], &cleanedNumber[len(cleanedNumber)-2])
+	}
+
+	return strings.Join(cleanedNumber, "")
+}
+
+func removeSpacesAndDashes(number string) string {
+	regularExression := regexp.MustCompile(`\d+`)
+	return strings.Join(regularExression.FindAllString(number, -1), "")
+
+}
+
+func swapValue(val1 *string, val2 *string) {
+	*val1, *val2 = *val2, *val1
+}
